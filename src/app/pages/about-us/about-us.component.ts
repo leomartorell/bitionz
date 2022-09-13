dcd52c66b1a1c9b5744e80aaf0d4b292fb0ac17c
Enzo Avalos <enzoavalos@cripto-country.com>
2022-09-13 16:34:50 -0300

    about-us page

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.scss']
+  styleUrls: ['./about-us.component.scss'],
 })
 export class AboutUsComponent implements OnInit {
+  innerWidth: number = 1080;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    let el = document.getElementById('top');
+    el?.scrollIntoView();
   }
-
 }
